64d9982815c5233f08ce5e30b0605945f9e60f9e
D <1le1248111@gmail.com>
2026-07-18 17:21:46 -0500

    comments changed

--- pointMaxxer.py
+++ pointMaxxer.py
@@ -1,22 +1,23 @@
-print("hi")
+print("hi, the code is starting")
 
-# figure out how to make daily set more consistent bc too unreliable
 import time
 import random
 import pyautogui
 import keyboard
-time.sleep(5)  # gives you 3 seconds to switch to the desired window
-SCALE = 1.5  # e.g., 150% scaling on Windows
+time.sleep(5)  # gives you 5 seconds to switch to the desired window
+SCALE = 1.5  # default 150% bc that's what i have on my comp 150% scaling on Windows
 
+# makes the move_to scaled for easier configuration (ive been using the script cursorCoords.py for coordinates)
 def scaled_move_to(x, y):
     pyautogui.moveTo(int(x * SCALE), int(y * SCALE))
 
 def scaled_click(x, y):
     pyautogui.click(int(x * SCALE), int(y * SCALE))
 
-#1150, 65
-scaled_move_to(567, 389)
+scaled_move_to(567, 389) # where the search bar is on my computer
 
+# searches up somethign different 35 times (35*5 = 175 pts, max pts from search daily is 150.)
+# 5 second wait bc there's a cooldown to how fast u can search stuff up
 for i in range(35) :
     scaled_click(135, 62)
     a = random.randint(1, 1000000000)
@@ -24,22 +25,23 @@ for i in range(35) :
 
     pyautogui.write(str(a) + " this is for microsoft reward points", interval=0.05)
 
-    time.sleep(3)
+    time.sleep(5)
 
     pyautogui.press("enter")
 
-scaled_click(140, 60)
+scaled_click(140, 60) # click back on search bar
 
 time.sleep(1)
-pyautogui.write("https://rewards.bing.com/", interval=0.05)
+pyautogui.write("https://rewards.bing.com/", interval=0.05) # go to ms rewards website
 time.sleep(1)
 pyautogui.press("enter")
 time.sleep(5)
 scaled_click(100, 4)
 time.sleep(1)
 
+# daily set sytem
 scaled_move_to(150, 570)
-print("a")
+print("daily set initalized")
 while True:
     x, y = pyautogui.position()
     screen_w, screen_h = pyautogui.size()
@@ -55,7 +57,7 @@ while True:
     if r == 16 and g == 124 and b == 16:
         print("green detected, exiting loop.")
         break
-
+# clicks the 3 daily set elements
 time.sleep(2)
 scaled_click(350, 630)
 time.sleep(1)
