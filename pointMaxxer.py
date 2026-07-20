1a70e226177d183b7b712a26cc8ded4b92bc4688
D <1le1248111@gmail.com>
2026-07-19 19:26:11 -0500

    bug fixes, some changes
    Updated the script to search for different items 20 times instead of 35 bc ms changed max from 150 to 100 (grr). moved sleep from before enter is pressed to after (fixed a bug where the events would trigger at weird times). also added check to see if the daily set dropdown if open. if it isn't, it opens it.

--- pointMaxxer.py
+++ pointMaxxer.py
@@ -3,8 +3,8 @@ print("hi, the code is starting")
 import time
 import random
 import pyautogui
-
-time.sleep(5)  # gives you 5 seconds to switch to the desired window
+import keyboard
+time.sleep(5)  # gives you 5 seconds  to switch to the desired window
 SCALE = 1.5  # default 150% bc that's what i have on my comp 150% scaling on Windows
 
 # makes the move_to scaled for easier configuration (ive been using the script cursorCoords.py for coordinates)
@@ -16,40 +16,34 @@ def scaled_click(x, y):
 
 scaled_move_to(567, 389) # where the search bar is on my computer
 
-# searches up somethign different 35 times (35*5 = 175 pts, max pts from search daily is 150.)
+# searches up somethign different 20 times (20*5 = 100 pts, max pts from search daily is 100.)
 # 5 second wait bc there's a cooldown to how fast u can search stuff up
-for i in range(35) :
+for i in range(20) :
     scaled_click(135, 62)
     a = random.randint(1, 1000000000)
     print(a)
 
     pyautogui.write(str(a) + " this is for microsoft reward points", interval=0.05)
 
-    time.sleep(5)
-
     pyautogui.press("enter")
+
+    time.sleep(3)
 
 scaled_click(140, 60) # click back on search bar
 
 time.sleep(1)
-pyautogui.write("https://rewards.bing.com/", interval=0.05) # go to ms rewards website
+pyautogui.write("https://rewards.bing.com/dashboard", interval=0.05) # go to ms rewards website
 time.sleep(1)
 pyautogui.press("enter")
 time.sleep(5)
-scaled_click(100, 4)
-time.sleep(1)
 
 # daily set sytem
 scaled_move_to(150, 570)
 print("daily set initalized")
 while True:
     x, y = pyautogui.position()
-    screen_w, screen_h = pyautogui.size()
 
-    x = max(0, min(x, screen_w - 1))
-    y = max(0, min(y, screen_h - 1))
-
-    r, g, b = pyautogui.pixel(x,y )
+    r, g, b = pyautogui.pixel(x,y)
     print(f"({r}, {g}, {b})")
     
     pyautogui.scroll(-10)
@@ -57,6 +51,17 @@ while True:
     if r == 16 and g == 124 and b == 16:
         print("green detected, exiting loop.")
         break
+
+scaled_move_to(300, 565)
+
+x, y = pyautogui.position()
+r, g, b = pyautogui.pixel(x,y)
+print(f"({r}, {g}, {b})")
+# detects if daily set dropdown is open, if not, it opens it
+if r == 28 and g == 34 and b == 49:
+    print("dropdown detected as closed, opening it.")
+    scaled_click(1200, 565)
+
 # clicks the 3 daily set elements
 time.sleep(2)
 scaled_click(350, 630)
